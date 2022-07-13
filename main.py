37230ea19e75cd20b84391b54ac8647793492b04
Kalasnikov Igor <snezhik0206@mail.ru>
2022-07-13 16:49:38 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_course.append(course_name)
+
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and \
+                course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+# lecturer = Lecturer("Oleg", "Bulygyn")
+# lecturer.courses_attached.append("Python")
+#
+# student = Student("Igor", "Kalashnikov", "Man")
+# student.courses_in_progress.append("Python")
+# student.rate_lecture(lecturer, "Python", 5)
+# student.rate_lecture(lecturer, "Python", 5)
+# print(lecturer.grades)
